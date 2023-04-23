f8c107d5eabafd145df0f2cbd76a2b668a7736e2
John Norén <super.happy.email.fun@gmail.com>
2023-04-23 18:00:48 +0200

    Improve performance

--- src/html/js/math/vector2.js
+++ src/html/js/math/vector2.js
@@ -1,18 +1,18 @@
 export class Vector2 {
-    constructor(x, y) {
+    constructor(x, y, magnitude) {
         this.x = x;
         this.y = y;
-        this.magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+        this.magnitude = magnitude || Math.sqrt(x * x + y * y);
     }
     normalized() {
-        const length = Math.sqrt(this.x * this.x + this.y * this.y);
-        const normX = this.x / length;
-        const normY = this.y / length;
+        const magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+        const normX = this.x / magnitude;
+        const normY = this.y / magnitude;
         return new Vector2(normX, normY);
     }
     static from(magnitude, direction) {
         const x = magnitude * direction.x;
         const y = magnitude * direction.y;
-        return new Vector2(x, y);
+        return new Vector2(x, y, magnitude);
     }
 }
--- src/ts/math/vector2.ts
+++ src/ts/math/vector2.ts
@@ -5,16 +5,16 @@ export class Vector2 {
     public readonly y: number;
     public readonly magnitude: number;
 
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, magnitude?: number) {
         this.x = x;
         this.y = y;
-        this.magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+        this.magnitude = magnitude || Math.sqrt(x * x + y * y);
     }
 
     public normalized(): Vector2 {
-        const length = Math.sqrt(this.x * this.x + this.y * this.y);
-        const normX = this.x / length;
-        const normY = this.y / length;
+        const magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+        const normX = this.x / magnitude;
+        const normY = this.y / magnitude;
 
         return new Vector2(normX, normY);
     }
@@ -23,7 +23,7 @@ export class Vector2 {
         const x = magnitude * direction.x;
         const y = magnitude * direction.y;
 
-        return new Vector2(x, y);
+        return new Vector2(x, y, magnitude);
     }
 
 }
